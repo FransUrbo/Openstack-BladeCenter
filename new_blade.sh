2f6ff46df009c75f6983681f5b0c6603f1f05eea
Turbo Fredriksson <turbo@bayour.com>
2016-06-11 21:15:06 +0100

    Put the /var/tmp/install stuff in the new_blade.sh instead.
    No need for TWO scripts..

--- new_blade.sh
+++ new_blade.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+# PXE auto installer - PART ONE
+
+do_install() {
+    apt-get -y --no-install-recommends install $*
+}
+
 set -ex
+
+export DEBIAN_FRONTEND="noninteractive"
 
 # Get the basic setup files (ssh/gpg keys etc)
 cd /var/tmp
-wget http://localserver/new_blade.tgz
+wget http://localserver/PXEBoot/new_blade.tgz
 cd /
 
 # Unpack the basic setup files
@@ -12,6 +20,37 @@ tar xvzf /var/tmp/new_blade.tgz
 
 # Make sure we can login as root
 sed -i "s@^PermitRootLogin@#PermitRootLogin@" /etc/ssh/sshd_config
+echo 'PubkeyAcceptedKeyTypes=+ssh-dss' >> /etc/ssh/sshd_config
 
-# Install core++ packages
-sh /var/tmp/install
+# Get extra repository keys
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8E234FB17DFFA34D # Turbo Fredriksson
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FADD8D64B1275EA3 # HPE
+
+apt-get update
+
+apt-get -y dist-upgrade
+
+hostname="$(cat /etc/hostname)"
+if echo "${hostname}" | grep -iq "^blade[A-Z][0-9][0-9]"; then
+    # A blade!
+    do_install hp-snmp-agents hponcfg debconf-utils ldap-utils open-iscsi dbconfig-mysql
+    sed -i "s@'false'@'true'@" /etc/dbconfig-common/config
+
+    # Need to do the rest of the install after a reboot, in the "real"
+    # OS. This because systemd refuses to start MySQL etc in the chroot.
+    sed -i "s@^\(exit.*\)\$@\
+# Discover all targets on Localserver.\\
+iscsiadm -m discovery -t st -p localserver:3260 > /dev/null\\
+#host=\"\$(hostname | sed \"s\@\(blade.*\)[a-z]\@\\\1\@\" | tr '[:upper:]' '[:lower:]')\"\\
+#iscsiadm -m node -p localserver:3260 -l \\\ \\
+#    -T iqn.2012-11.tld.domain:share.virtualmachines.blade.center.1.\${host}\\
+\\
+# Run part two of the PXE auto installer.\\
+sh /var/tmp/rc.install &\\
+\\
+exit 0\\
+@" /etc/rc.local
+fi
+
+apt-get -y autoremove
+apt-get -y clean
