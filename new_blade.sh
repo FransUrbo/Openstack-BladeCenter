23db6ee0db7f6c35fed78e8f66ba1913d42f6b6d
Turbo Fredriksson <turbo@bayour.com>
2016-06-12 11:46:42 +0100

    Escape the '&' on the 'run rc.install' line.

--- new_blade.sh
+++ new_blade.sh
@@ -33,14 +33,14 @@ apt-get -y dist-upgrade
 hostname="$(cat /etc/hostname)"
 if echo "${hostname}" | grep -iq "^blade[A-Z][0-9][0-9]"; then
     # A blade!
-    do_install hp-snmp-agents hponcfg debconf-utils ldap-utils open-iscsi dbconfig-mysql
+    do_install hp-snmp-agents hponcfg dbconfig-mysql
     sed -i "s@'false'@'true'@" /etc/dbconfig-common/config
 
     # Need to do the rest of the install after a reboot, in the "real"
     # OS. This because systemd refuses to start MySQL etc in the chroot.
     sed -i "s@^\(exit.*\)\$@\
 # Discover all targets on Localserver.\\
-iscsiadm -m discovery -t st -p localserver:3260 > /dev/null\\
+#iscsiadm -m discovery -t st -p localserver:3260 > /dev/null\\
 #host=\"\$(hostname | sed \"s\@\(blade.*\)[a-z]\@\\\1\@\" | tr '[:upper:]' '[:lower:]')\"\\
 #iscsiadm -m node -p localserver:3260 -l \\\ \\
 #    -T iqn.2012-11.tld.domain:share.virtualmachines.blade.center.1.\${host}\\
