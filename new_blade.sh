b04ac2ad5db11cbd633b56f5186e4aac5ff66ba5
Turbo Fredriksson <turbo@bayour.com>
2016-06-14 21:51:25 +0100

    Do the apt-get update/dist-upgrade in rc.install instead.
    * Escape the '&' at the end of the call to rc.install so it is
      added to the file (which it wasn't).

--- new_blade.sh
+++ new_blade.sh
@@ -26,27 +26,24 @@ echo 'PubkeyAcceptedKeyTypes=+ssh-dss' >> /etc/ssh/sshd_config
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8E234FB17DFFA34D # Turbo Fredriksson
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FADD8D64B1275EA3 # HPE
 
-apt-get update
-
-apt-get -y dist-upgrade
-
 hostname="$(cat /etc/hostname)"
 if echo "${hostname}" | grep -iq "^blade[A-Z][0-9][0-9]"; then
     # A blade!
+
     do_install hp-snmp-agents hponcfg dbconfig-mysql
     sed -i "s@'false'@'true'@" /etc/dbconfig-common/config
 
     # Need to do the rest of the install after a reboot, in the "real"
     # OS. This because systemd refuses to start MySQL etc in the chroot.
-    sed -i "s@^\(exit.*\)\$@\
+    sed -i "s@^exit.*@\
 # Discover all targets on Localserver.\\
 #iscsiadm -m discovery -t st -p localserver:3260 > /dev/null\\
 #host=\"\$(hostname | sed \"s\@\(blade.*\)[a-z]\@\\\1\@\" | tr '[:upper:]' '[:lower:]')\"\\
 #iscsiadm -m node -p localserver:3260 -l \\\ \\
-#    -T iqn.2012-11.tld.domain:share.virtualmachines.blade.center.1.\${host}\\
+#    -T iqn.2012-11.tld.domain:share.virtualmachines.blade.center.\${host}\\
 \\
 # Run part two of the PXE auto installer.\\
-sh /var/tmp/rc.install &\\
+sh /var/tmp/rc.install \&\\
 \\
 exit 0\\
 @" /etc/rc.local
