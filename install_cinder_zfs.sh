8d7c064c5a33754a8818ab386ccfea0f5dcf8f4a
Turbo Fredriksson <turbo@bayour.com>
2016-06-15 20:32:02 +0100

    Updates on the install_cinder_zfs script.
    * Load functions from openstack-pkg-tools.
    * Get IP and hostname.
    * Get the modified tparker+mariocar zol.py script.
    * Use pkgos_inifile instead of sed to update config file.

--- install_cinder_zfs.sh
+++ install_cinder_zfs.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
+# https://www.logilab.org/blogentry/114769
+# https://github.com/tparker00/Openstack-ZFS
+
 exit 0 # Not yet..
 set -ex
 
+if [ ! -e /usr/share/openstack-pkg-tools/pkgos_func ]; then
+    echo "ERROR: openstack-pkg-tools not installed"
+    exit 1
+fi
+
+. /usr/share/openstack-pkg-tools/pkgos_func
+export PKGOS_VERBOSE=yes
+
+# Get IP of this host
+set -- $(/sbin/ip address | egrep " eth.* UP ")
+iface="$(echo "${2}" | sed 's@:@@')"
+set -- $(/sbin/ifconfig "${iface}" | grep ' inet ')
+ip="$(echo "${2}" | sed 's@.*:@@')"
+
+# Get the hostname. This is the simplest and fastest.
+hostname="$(cat /etc/hostname)"
+
+# ======================================================================
+
 # Get the Cinder-ZFS/ZoL plugin
-curl -s https://raw.githubusercontent.com/tparker00/Openstack-ZFS/master/zol.py > \
+#curl -s https://raw.githubusercontent.com/tparker00/Openstack-ZFS/master/zol.py > \
+curl -s http://${LOCALSERVER}/PXEBoot/nova-zol.py-tparker+mariocar > \
     /usr/lib/python2.7/dist-packages/cinder/volume/zol.py
 
-# Get the ZFS/ZoL wrapper (?)
-curl -s http://${LOCALSERVER}/PXEBoot/var/www/PXEBoot/zfswrapper > \
+# Get the ZFS/ZoL wrapper - ?? On the SAN server or here ??
+curl -s http://${LOCALSERVER}/PXEBoot/zfswrapper > \
     /usr/local/sbin/zfswrapper
 chmod +x /usr/local/sbin/zfswrapper
 
@@ -21,6 +44,7 @@ EOF
 
 cat <<EOF >> /etc/cinder/cinder.conf
 
+# ZFS/ZoL driver - https://github.com/tparker00/Openstack-ZFS
 [zol]
 volume_driver=cinder.volume.zol.ZFSonLinuxISCSIDriver
 volume_group=share/BladeCenter
@@ -31,13 +55,15 @@ san_ip=${ip}
 san_zfs_volume_base=share/BladeCenter
 san_is_local=false
 san_login=root
-san_private_key=/etc/nova/sankey
+san_private_key=/etc/nova/sshkey
 use_cow_images=false
 san_zfs_command=/usr/local/sbin/zfswrapper
 verbose=true
 EOF
 
-sed -i "s@^\(enabled_backends[ \t]=.*\)@\1, zol" /etc/cinder/cinder.conf
+backends="$(grep '^enabled_backends' /etc/cinder/cinder.conf | sed 's@[ \t].*@@')"
+[ -n "${backends}" ] && backends="${backends},"
+pkgos_inifile set /etc/cinder/cinder.conf DEFAULT enabled_backends ${backends}zol
 for init in /etc/init.d/cinder-*; do $init restart; done
 
 # Get the Cinder ZFS/ZoL ssh keys to use with ZFS/ZoL SAN.
