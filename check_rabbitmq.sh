967878a793af074ded90127139d308429a67fdf6
Turbo Fredriksson <turbo@bayour.com>
2016-06-27 23:43:44 +0100

    Use full path to rabbitmqctl.

--- check_rabbitmq.sh
+++ check_rabbitmq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh                                                                                                                                                     
 
-if ! rabbitmqctl -t 10 list_users | grep -q openstack; then
+if ! /usr/sbin/rabbitmqctl -t 10 list_users | grep -q openstack; then
     echo "Not running, restarting."
     /etc/init.d/rabbitmq-server start
 fi
