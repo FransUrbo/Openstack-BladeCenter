110b6ad0adaad5d13d30764eda213250d23754ba
Turbo Fredriksson <turbo@bayour.com>
2016-06-30 09:49:04 +0100

    Be smarter about how we detect if RabbitMQ have crashed.

--- check_rabbitmq.sh
+++ check_rabbitmq.sh
@@ -1,6 +1,10 @@
 #!/bin/sh                                                                                                                                                     
 
-if ! /usr/sbin/rabbitmqctl -t 10 list_users | grep -q openstack; then
+if ! /usr/sbin/rabbitmqctl -t 10 list_users > /dev/null 2>&1 ; then
+    echo "Can't connect. Just make sure it's not actually running."
+    cnt="$(/bin/ps faxwww | grep rabbitmq-server | grep -v grep | wc -l)"
+    [ "${cnt}" -eq 2 ] || /etc/init.d/rabbitmq-server stop
+
     echo "Not running, restarting."
     /etc/init.d/rabbitmq-server start
 fi
