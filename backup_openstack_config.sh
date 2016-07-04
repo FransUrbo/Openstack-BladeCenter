13200a2750581d51cc3011e50c3bf6a4dba795c9
Turbo Fredriksson <turbo@bayour.com>
2016-07-04 15:56:33 +0100

    backup_openstack_config: Create backup file in /var/backups.
    * Suffixed by the current date and prefixed with 'openstack-configs-'.
    * Allow for keeping the temporary directory with 'no-clean' as opt param.

--- backup_openstack_config.sh
+++ backup_openstack_config.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-rm -Rf /tmp/openstack
+OPTS="${*}"
+
+DATE="$(date +"%Y%m%d_%H%M")"
+
+echo "Backing up Openstack databases at ${DATE}:"
 find /etc -name '*.orig' -o -name '*~' | \
     sed -e 's@\.orig@@' -e 's@~@@' | \
     sort | uniq | \
     while read file; do
 	echo "${file}" | \
-            egrep -q "network/interfaces" && \
+            egrep -q "network/interfaces|\.lock" && \
             continue
 
         echo "${file}"
 
-	dir="/tmp/openstack/$(dirname "${file}")"
+	dir="/tmp/openstack.$$/$(dirname "${file}")"
         mkdir -p "${dir}"
 
         grep '^[a-z\[]' "${file}" > "${dir}/$(basename "${file}")"
     done
+
+cd /tmp/openstack.$$
+tar czf /var/backups/openstack-configs-${DATE}.tar.gz \
+    $(find -type f | sort)
+
+echo "${OPTS}" | grep -q "no-clean" || \
+    rm -Rf /tmp/openstack.$$
